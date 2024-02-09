a6f30134a3a07d57ebc24c2013b63de3105ddcf5
OninAina <hei.oninaina@gmail.com>
2024-02-09 23:15:11 +0200

    add navbar

---
+++ kotikota-ui/src/app/(pages)/navbar/page.js
@@ -0,0 +1,22 @@
+import './navbar.css'
+const Navbar = () => {
+
+    return (
+        <>
+            <nav>
+                <img class="logo" src="/images/mainLogo.png" alt="Logo" />
+                <div class="options">
+                    <div class="login">
+                        <img class="user" src="/images/login.png" alt="" />
+                        <a href="">SE CONNECTER</a>
+                    </div>
+                    <div class="search">
+                        <img class="research" src="/images/mainSearch.png" alt="research" />
+                        <input type="text" placeholder="Rechercher" />
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+export default Navbar;
