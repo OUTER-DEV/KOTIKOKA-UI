a746c91c54f9c5e2796a8a9bc31eb6731ec346f2
OninAina <hei.oninaina@gmail.com>
2024-02-10 06:58:02 +0200

    update loading and add backgroundLogin

--- kotikota-ui/src/app/(pages)/navbar/page.js
+++ kotikota-ui/src/app/(pages)/navbar/page.js
@@ -4,14 +4,14 @@ const Navbar = ()=>{
     return(
         <>
            <nav>
-            <img class="logo" src="mainLogo.png" alt="Logo"/>
+            <img class="logo" src="/images/mainLogo.png" alt="Logo"/>
             <div class="options">
                 <div class="login">
-                    <img class="user" src="login.png" alt="user"/>
+                    <img class="user" src="/images/login.png" alt="user"/>
                     <a href="">SE CONNECTER</a>
                 </div>
                 <div class="search">
-                    <img class="research" src="mainSearch.png" alt="research"/>
+                    <img class="research" src="/images/mainSearch.png" alt="research"/>
                     <input type="text" placeholder="Rechercher"/>
                 </div>
             </div>
