5a844b72157333211ba4e185943e91a1c9467dd2
njaina <hei.njaina.2@gmail.com>
2024-02-10 06:11:02 +0300

    feat: update api provider

---
+++ kotikota-ui/src/app/providers/projects/project.js
@@ -0,0 +1,43 @@
+"use client"
+import axios from "axios"
+
+const app = axios.create({
+    headers:{
+      'Content-Type': 'application/json',
+    }
+  })
+
+const baseURL = process.env.NEXT_PUBLIC_URL+"/Projects"
+export const dataProvider = {
+
+launchProject: async(project) =>{
+const newProject = await app.post(baseURL+"/launch",{
+
+  "title": project.title,
+  "description":project.description,
+  "category": project.category,
+  "targetAmount": project.targetAmount,
+  "balance": project.balance,
+  "deadline":project.deadline,
+  "percentageAchieved": project.percentageAchieved,
+  "numberOfContributors": project.numberOfContributors,
+  "username": project.owner.username,
+  "name":project.owner.name,
+  "email":project.owner.email,
+  "role": project.owner.role
+})
+return newProject;
+},
+getAll:async (accessToken)=>{
+    return app.get(baseURL+"/all?", {
+         headers:{
+        Authorization:"Bearer "+accessToken
+      }
+    }).then((data)=>{
+        return data.data;
+    }).catch((e)=>{
+         throw e;
+        })
+},
+
+}
