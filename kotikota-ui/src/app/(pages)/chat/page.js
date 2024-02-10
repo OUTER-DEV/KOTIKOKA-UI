73aa403fd3c0f14e74e2c1f574b510a0cc3d1d4a
OninAina <hei.oninaina@gmail.com>
2024-02-10 18:06:56 +0200

    add chat

---
+++ kotikota-ui/src/app/(pages)/chat/page.js
@@ -0,0 +1,69 @@
+import './chat.css'
+
+const Chat = () =>{
+    return(
+        <>
+        <body>
+    <header>
+        <img src="/images/mainLogo.png" alt="logo"/>
+        <h1>Messagerie</h1>
+    </header>
+    
+    <aside>
+        <button class="OneMessage">
+            <img src="/images/login.png" alt="user"/>
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+           
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        <button class="OneMessage">
+        <img src="/images/login.png" alt="user"/>
+            
+            <p>message(tsy aiko)</p>
+        </button>
+        
+
+    </aside>
+
+    <div class="messages"></div>
+    
+    
+</body>
+        </>
+    )
+}
+
+export default Chat;
